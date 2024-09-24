c725081a01ee1d5a2278223b18d083dc38ac092c
Gohitha6Nadikota <gohithapriyanadikota@gmail.com>
2024-09-24 16:46:24 +0530

    Added functionality

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,16 @@
 "use client";
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
 // Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
@@ -14,9 +20,6 @@ const links = [
   },
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
 ];
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
 
 export default function NavLinks() {
   const pathname = usePathname();
